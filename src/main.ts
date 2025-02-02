05fdb6c9afc52bee03b7f583611f3bfc6e8bbc87
khiemldk <khiemld28@gmail.com>
2025-02-02 17:04:14 +0700

    feat: add basic docker

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { abortOnError: false });
-  await app.listen(3000);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      // Cái này dùng để white list DTO khi gửi lên, nếu fields không được khai báo trong DTO thì sẽ bị loại bỏ
+      whitelist: true,
+    }),
+  );
+  await app.listen(2802);
 }
 bootstrap();
