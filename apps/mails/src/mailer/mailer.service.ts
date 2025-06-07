1880d352240f4293845b1f12792a85f3042057d3
khiemldk <khiemld28@gmail.com>
2025-06-07 23:44:21 +0700

    fix: nodemailer smtp

--- apps/mails/src/mailer/mailer.service.ts
+++ apps/mails/src/mailer/mailer.service.ts
@@ -1,5 +1,5 @@
 import { ConfigService } from '@nestjs/config';
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import * as Nodemailer from 'nodemailer';
 import * as fs from 'node:fs/promises';
 import Handlebars from 'handlebars';
@@ -7,13 +7,31 @@ import Handlebars from 'handlebars';
 @Injectable()
 export class MailerService {
   private readonly transporter: Nodemailer.Transporter;
+  protected readonly logger = new Logger(MailerService.name);
   constructor(private readonly configService: ConfigService) {
     this.transporter = Nodemailer.createTransport({
-      service: this.configService.get<string>('MAIL_SERVICE', { infer: true }),
+      host: this.configService.get<string>('MAIL_HOST', { infer: true }),
+      port: this.configService.get<number>('MAIL_PORT', { infer: true }),
+      secure: this.configService.get<boolean>('MAIL_SECURE', { infer: true }),
+      ignoreTLS: this.configService.get<boolean>('MAIL_IGNORE_TLS', {
+        infer: true,
+      }),
+      requireTLS: this.configService.get<boolean>('MAIL_REQUIRE_TLS', {
+        infer: true,
+      }),
       auth: {
         user: this.configService.get<string>('MAIL_USER', { infer: true }),
         pass: this.configService.get<string>('MAIL_PASSWORD', { infer: true }),
       },
+      logger: true,
+      debug: true,
+    });
+    this.transporter.verify((error) => {
+      if (error) {
+        this.logger.error('Error connect SMTP', error);
+      } else {
+        this.logger.log('Connect SMTP successfully');
+      }
     });
   }
 
