6021af2b6082f40332b1e83f1794eb4a0bedb48c
khiemldk <khiemld28@gmail.com>
2025-06-08 00:17:46 +0700

    feat: update nodemailer to sendgrid api

--- apps/mails/src/mailer/mailer.service.ts
+++ apps/mails/src/mailer/mailer.service.ts
@@ -1,61 +1,55 @@
 import { ConfigService } from '@nestjs/config';
 import { Injectable, Logger } from '@nestjs/common';
-import * as Nodemailer from 'nodemailer';
 import * as fs from 'node:fs/promises';
 import Handlebars from 'handlebars';
+import * as sgMail from '@sendgrid/mail';
 
 @Injectable()
 export class MailerService {
-  private readonly transporter: Nodemailer.Transporter;
   protected readonly logger = new Logger(MailerService.name);
+
   constructor(private readonly configService: ConfigService) {
-    this.transporter = Nodemailer.createTransport({
-      host: this.configService.get<string>('MAIL_HOST', { infer: true }),
-      port: this.configService.get<number>('MAIL_PORT', { infer: true }),
-      secure: this.configService.get<boolean>('MAIL_SECURE', { infer: true }),
-      ignoreTLS: this.configService.get<boolean>('MAIL_IGNORE_TLS', {
-        infer: true,
-      }),
-      requireTLS: this.configService.get<boolean>('MAIL_REQUIRE_TLS', {
-        infer: true,
-      }),
-      auth: {
-        user: this.configService.get<string>('MAIL_USER', { infer: true }),
-        pass: this.configService.get<string>('MAIL_PASSWORD', { infer: true }),
-      },
-      logger: true,
-      debug: true,
+    const apiKey = this.configService.get<string>('MAIL_PASSWORD', {
+      infer: true,
     });
-    this.transporter.verify((error) => {
-      if (error) {
-        this.logger.error('Error connect SMTP', error);
-      } else {
-        this.logger.log('Connect SMTP successfully');
-      }
-    });
+    if (!apiKey) {
+      this.logger.error('Missing SENDGRID_API_KEY');
+      return;
+    }
+
+    sgMail.setApiKey(apiKey);
+    this.logger.log('SendGrid Mailer initialized');
   }
 
   async sendMail({
     templatePath,
     context,
     ...mailOptions
-  }: Nodemailer.SendMailOptions & {
+  }: {
     templatePath: string;
     context: Record<string, any>;
+    to: string;
+    subject: string;
+    from?: string;
   }): Promise<void> {
-    let html: string | undefined;
-    if (templatePath) {
+    try {
       const template = await fs.readFile(templatePath, 'utf-8');
-      html = Handlebars.compile(template, {
-        strict: true,
-      })(context);
+      const html = Handlebars.compile(template)(context);
+
+      await sgMail.send({
+        to: mailOptions.to,
+        from: mailOptions?.from
+          ? mailOptions.from
+          : (this.configService.get<string>('MAIL_FROM', {
+              infer: true,
+            }) as string),
+        subject: mailOptions.subject,
+        html,
+      });
+
+      this.logger.log(`Email sent to ${mailOptions.to}`);
+    } catch (error) {
+      this.logger.error('Failed to send email', error);
     }
-    this.transporter.sendMail({
-      ...mailOptions,
-      from: mailOptions?.from
-        ? mailOptions.from
-        : this.configService.get('MAIL_FROM'),
-      html: mailOptions.html ? mailOptions.html : html,
-    });
   }
 }
